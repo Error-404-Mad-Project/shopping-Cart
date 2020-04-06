80037acb1c9e228e832fb7f024407fc094d744e9
SenuraKeheliya <senurakeheliya44@gmail.com>
2020-04-06 20:43:39 +0530

    senura

---
+++ app/src/main/java/com/shoppingcart/acivity_commentView.java
@@ -0,0 +1,14 @@
+package com.shoppingcart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class acivity_commentView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acivity_comment_view);
+    }
+}
