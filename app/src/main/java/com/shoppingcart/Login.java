d846283e120a821b9eb8edea84d4b91160d4f9aa
Kavindu Hasaranga <53331162+KavHasrana@users.noreply.github.com>
2020-04-14 23:06:29 +0530

    Database Connection

--- app/src/main/java/com/shoppingcart/Login.java
+++ app/src/main/java/com/shoppingcart/Login.java
@@ -2,6 +2,7 @@ package com.shoppingcart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,19 +10,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.shoppingcart.Database.DatabaseHelper;
+
 //import com.shoppingcart.Prevalent.Prevalent;
 
 public class Login extends AppCompatActivity {
+
     EditText mTextUsername;
     EditText mTextPassword;
     Button mButtonRegister;
     Button mButtonLogin;
+    DatabaseHelper database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        database = new DatabaseHelper (this);
         mTextUsername = (EditText) findViewById (R.id.editText);
         mTextPassword = (EditText) findViewById (R.id.editText3);
         mButtonRegister = (Button) findViewById(R.id.button3);
@@ -31,20 +38,31 @@ public class Login extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             // Do something in response to button
-            Intent intent = new Intent (Login.this, RegistrationForm.class);
-            startActivity (intent);
+            Intent registerIntent = new Intent (Login.this, RegistrationForm.class);
+            startActivity (registerIntent);
         }
         });
 
         mButtonLogin.setOnClickListener (new View.OnClickListener () { /** Called when the user taps the Login button */
         @Override
         public void onClick(View v) {
-            // Do something in response to button
-            Intent intent = new Intent (Login.this,CustomerView.class);
-//          Prevalent.currentOnlineUser = usersData;
-            startActivity (intent);
 
-            Toast.makeText (Login.this,"Logged in Successfully",Toast.LENGTH_SHORT).show ();
+            String user = mTextUsername.getText ().toString ().trim ();
+            String pwd = mTextPassword.getText ().toString ().trim ();
+            Boolean res = database.checkUser (user,pwd);
+            if(res == true)
+            {
+                // Do something in response to button
+                Intent CustView = new Intent (Login.this,CustomerView.class);
+//          Prevalent.currentOnlineUser = usersData;
+                startActivity (CustView);
+
+                Toast.makeText (Login.this,"Logged in Successfully",Toast.LENGTH_SHORT).show ();
+            }
+            else
+            {
+                Toast.makeText (Login.this,"Login Error",Toast.LENGTH_SHORT).show();
+            }
 
                 }
             });
--- app/src/main/java/com/shoppingcart/RegistrationForm.java
+++ app/src/main/java/com/shoppingcart/RegistrationForm.java
@@ -15,18 +15,22 @@ import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.shoppingcart.Database.DatabaseHelper;
 
 public class RegistrationForm extends AppCompatActivity {
 
+    DatabaseHelper database;
     EditText userName;
     EditText emailAddress;
     EditText address;
     EditText phoneNumber;
     EditText password;
     EditText confirmPassword;
+    private ProgressDialog loadingBar;
 
     private Button nextPage;
-    private ProgressDialog loadingBar;
 
     private static final String CHANNEL_ID = String.valueOf(0);
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -34,21 +38,27 @@ public class RegistrationForm extends AppCompatActivity {
     public static final String EXTRA_MESSAGE2 = "Third Message";
     public static final String EXTRA_MESSAGE3 = "Fourth Message";
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_registration_form);
 
+        database = new DatabaseHelper (this);
+        userName = (EditText)findViewById (R.id.editText5);
+        emailAddress = (EditText)findViewById (R.id.editText7);
+        address = (EditText)findViewById (R.id.editText8);
+        phoneNumber = (EditText)findViewById (R.id.editText9);
+        password = (EditText)findViewById (R.id.editText10);
+        confirmPassword = (EditText)findViewById (R.id.editText11);
         loadingBar = new ProgressDialog(this);
         nextPage = (Button) findViewById(R.id.button);
         nextPage.setOnClickListener (new View.OnClickListener () { /** Called when the user taps the Register button */
 
+
         @Override
         public void onClick(View v) {
             // Do something in response to button
-            Intent intent = new Intent (RegistrationForm.this,CustomerView.class);
+            Intent LoginIntent = new Intent (RegistrationForm.this,Login.class);
 
             EditText editText5 = (EditText) findViewById (R.id.editText5);
             EditText editText7 = (EditText) findViewById (R.id.editText7);
@@ -60,28 +70,57 @@ public class RegistrationForm extends AppCompatActivity {
             String message2 =editText8.getText ().toString ();
             String message3 =editText9.getText ().toString ();
 
-            intent.putExtra (EXTRA_MESSAGE, message);
-            intent.putExtra (EXTRA_MESSAGE1, message1);
-            intent.putExtra (EXTRA_MESSAGE2, message2);
-            intent.putExtra (EXTRA_MESSAGE3, message3);
-            startActivity (intent);
+            LoginIntent.putExtra (EXTRA_MESSAGE, message);
+            LoginIntent.putExtra (EXTRA_MESSAGE1, message1);
+            LoginIntent.putExtra (EXTRA_MESSAGE2, message2);
+            LoginIntent.putExtra (EXTRA_MESSAGE3, message3);
+            startActivity (LoginIntent);
 
+        }
+        });
 
-            /**Create Loading Bar**/
-            loadingBar.setTitle ("Create Account");
-            loadingBar.setMessage ("Please wait, while we are checking the credentials");
-            loadingBar.setCanceledOnTouchOutside (false);
-            loadingBar.show ();
+            nextPage.setOnClickListener (new View.OnClickListener (){
+                @Override
+                public void onClick(View view) {
+                    String user = userName.getText ().toString ().trim ();
+                    String email = emailAddress.getText ().toString ().trim ();
+                    String add = address.getText ().toString ().trim ();
+                    String number = phoneNumber.getText ().toString ().trim ();
+                    String pwd = password.getText ().toString ().trim ();
+                    String cnf_pwd = confirmPassword.getText ().toString ().trim ();
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                CharSequence name = "shopping-Cart";
-                String description = "shopforme";
-                int importance = NotificationManager.IMPORTANCE_DEFAULT;
-                NotificationChannel channel = new NotificationChannel (CHANNEL_ID, name, importance);
-                channel.setDescription (description); // Register the channel with the system; you can't change the importance or other notification behaviors after this
-                NotificationManager notificationManager = getSystemService (NotificationManager.class);
-                notificationManager.createNotificationChannel (channel);
-            }
+                    if (pwd.equals (cnf_pwd)){
+                        long val = database.addUser (user,pwd);
+                        if (val > 0) {
+                            Toast.makeText (RegistrationForm.this, "Register Successfully", Toast.LENGTH_SHORT).show ();
+                            Intent moveToLogin = new Intent (RegistrationForm.this, Login.class);
+                            startActivity (moveToLogin);
+
+                            /**Create Loading Bar**/
+                            loadingBar.setTitle ("Create Account");
+                            loadingBar.setMessage ("Please wait, while we are checking the credentials");
+                            loadingBar.setCanceledOnTouchOutside (false);
+                            loadingBar.show ();
+                        }
+                        else {
+                            Toast.makeText (RegistrationForm.this, "Registration Error", Toast.LENGTH_SHORT).show ();
+                        }
+                    }
+                    else {
+                        Toast.makeText (RegistrationForm.this,"Password is not matching",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+//
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                CharSequence name = "shopping-Cart";
+//                String description = "shopforme";
+//                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//                NotificationChannel channel = new NotificationChannel (CHANNEL_ID, name, importance);
+//                channel.setDescription (description); // Register the channel with the system; you can't change the importance or other notification behaviors after this
+//                NotificationManager notificationManager = getSystemService (NotificationManager.class);
+//                notificationManager.createNotificationChannel (channel);
+//            }
 
 //            EditText text = (EditText) findViewById (R.id.editText5);
 //            String message1 = text.getText ().toString ();
@@ -97,8 +136,7 @@ public class RegistrationForm extends AppCompatActivity {
 //                    .setAutoCancel(true);
 //            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this); // notificationId is a unique int for each notification that you must define
 //            notificationManager.notify(0, builder.build());
-        }
-        });
+
 
 
     }
