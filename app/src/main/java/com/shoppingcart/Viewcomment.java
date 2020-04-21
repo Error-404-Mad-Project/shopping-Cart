4dad0d10268d1bcf26b7e4d710e56ddaff985951
ayeshani <ayeshanikaushalya@gmail.com>
2020-04-21 14:53:13 +0530

    write comment with firebase

--- app/src/main/java/com/shoppingcart/Viewcomment.java
+++ app/src/main/java/com/shoppingcart/Viewcomment.java
@@ -1,20 +1,37 @@
 package com.shoppingcart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.rey.material.widget.LinearLayout;
 import com.rey.material.widget.RelativeLayout;
 
 public class Viewcomment extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private DatabaseReference Usersref,PostRef;
+
+    private String Post_key,current_user_id;
+
     private RecyclerView commentList;
     private Button button_write;
+    private String commentText;
 
 
 
@@ -23,15 +40,33 @@ public class Viewcomment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewcomment);
 
+        PostRef = FirebaseDatabase.getInstance().getReference().child("Post").child(Post_key).child("Comments");
+        Usersref = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        button_write=(Button)findViewById(R.id.btn_write);
+
         commentList = (RecyclerView)findViewById(R.id.comment_List);
         commentList.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setReverseLayout(true);
         linearLayoutManager.setStackFromEnd(true);
         commentList.setLayoutManager(linearLayoutManager);
+
+
+        button_write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendViewToCommentActivity();
+            }
+
+            private void sendViewToCommentActivity() {
+                Intent addnewMessage = new Intent(Viewcomment.this,Writecomment.class);
+                startActivity(addnewMessage);
+            }
+        });
     }
 
-    public void onButtonClickListener(){
+    /*public void onButtonClickListener(){
         button_write = (Button)findViewById(R.id.btn_write);
         button_write.setOnClickListener(
                 new android.view.View.OnClickListener() {
@@ -41,7 +76,83 @@ public class Viewcomment extends AppCompatActivity {
                         startActivity(intent);
                     }
                 });
+    }*/
+
+    /*Comments.class,
+                R.layout.all_comments_layout,
+                commentViewHolder.class,
+                PostRef*/
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Comments>options =
+                new FirebaseRecyclerOptions.Builder<Comments>()
+                .setQuery(PostRef,Comments.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Comments,commentViewHolder> firebaseRecyclerAdapter
+                =new FirebaseRecyclerAdapter<Comments,commentViewHolder>(options){
+            @NonNull
+            @Override
+            public commentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.all_comments_layout,parent,false);
+                commentViewHolder viewHolder = new commentViewHolder(view);
+                return  viewHolder;
+
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull commentViewHolder viewHolder, int position, @NonNull Comments model) {
+                viewHolder.setUsername(model.getUsername());
+                viewHolder.setComment(model.getComment());
+                viewHolder.setDate(model.getDate());
+                viewHolder.setTime(model.getTime());
+
+            }
+
+        };
+        commentList.setAdapter(firebaseRecyclerAdapter);
+        firebaseRecyclerAdapter.startListening();
+        }
+
+
+    public static class commentViewHolder extends RecyclerView.ViewHolder
+    {
+        View mView;
+
+        public commentViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mView = itemView;
+        }
+
+        public void setUsername(String username)
+        {
+            TextView myuserName = (TextView)mView.findViewById(R.id.comment_Username);
+            myuserName.setText("@"+username + " ");
+        }
+
+        public void setComment(String comment)
+        {
+            TextView myComment = (TextView)mView.findViewById(R.id.comment_Text);
+            myComment.setText(comment);
+        }
+
+        public void setDate(String date)
+        {
+            TextView myData = (TextView)mView.findViewById(R.id.comment_date );
+            myData.setText(" Date: "+date);
+        }
+
+        public void setTime(String time)
+        {
+            TextView myTime = (TextView)mView.findViewById(R.id.comment_time);
+            myTime.setText(" Time: "+time);
+        }
     }
+
 
 
 }
--- app/src/main/java/com/shoppingcart/Writecomment.java
+++ app/src/main/java/com/shoppingcart/Writecomment.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.text.TextUtils;
@@ -12,43 +14,57 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 public class Writecomment extends AppCompatActivity {
 
 
    // private StorageReference PostMassageReference;
-    private DatabaseReference PostRef;
-    private FirebaseAuthException mAuth;
+    private FirebaseAuth mAuth;
+    private DatabaseReference Usersref,PostRef;
 
     private String Post_key,current_user_id;
 
     private Button postCommentButton;
     private EditText commentInputText;
+    private String commentText;
+    private Uri texturi;
 
-
-    //private String saveCurrentDate,saveCurrentTime;
-
-
+    private String saveCurrentDate,saveCurrentTime,RandomKey;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writecomment);
 
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        //DatabaseReference myRef = database.getReference("message");
 
        // PostMassageReference = FirebaseStorage.getInstance().getReference();
 
-        Post_key = getIntent().getExtras().get("PostKey").toString();
-        PostRef = FirebaseDatabase.getInstance().getReference().child("Post").child(Post_key);
+       Post_key = getIntent().getExtras().get("PostKey").toString();
 
-        //mAuth=FirebaseAuthException.getInstence();
+       mAuth= FirebaseAuth.getInstance();
+       current_user_id = mAuth.getCurrentUser().getUid();
+       Usersref = FirebaseDatabase.getInstance().getReference().child("Users");
+       PostRef = FirebaseDatabase.getInstance().getReference().child("Post").child(Post_key).child("Comments");
+
 
         commentInputText=(EditText)findViewById(R.id.comment_Input);
         postCommentButton=(Button) findViewById(R.id.post_comment_btn);
@@ -56,17 +72,52 @@ public class Writecomment extends AppCompatActivity {
         postCommentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                     commentInputText.setText("");
+
+               // validatePostInfo();
+
+                Usersref.child(current_user_id).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists()){
+                            String username = dataSnapshot.child("username").getValue().toString();
+
+                            validatecomment(username);
+                            commentInputText.setText("");
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
             }
-        });
-        
-       // @Override
-              //  public void onCancelled(DatabaseError databaseError){
-            
-       //  }
+
+          /*  private void validatePostInfo() {
+                commentText = commentInputText.getText().toString();
+
+                 commentText = commentInputText.getText().toString();
+                if (TextUtils.isEmpty(commentText)) {
+                   //Toast.makeText(this, "please write text to comment...", LENGTH_SHORT).show();
+
+                } else {
+
+                }
+            }*/
 
 
+            });
+/*
+            private void validatePostInfo() {
+                String commentTexte = commentInputText.getText().toString();
 
+                String commentText = commentInputText.getText().toString();
+                if(TextUtils.isEmpty(commentText)){
+                    Toast.makeText(this,"please write text to comment...", LENGTH_SHORT).show();
+
+                }
 
        /* public static class  CommentsViewHolder extends RecyclerView.ViewHolder{
             view mView;
@@ -76,13 +127,12 @@ public class Writecomment extends AppCompatActivity {
             }
         }*/
 
-
-        String commentText = commentInputText.getText().toString();
+      /*  String commentText = commentInputText.getText().toString();
         if(TextUtils.isEmpty(commentText)){
-            Toast.makeText(this,"please write text to comment...", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"please write text to comment...", LENGTH_SHORT).show();
 
-        }
-        else{
+        }*/
+      /*  else {
            /* Calendar calForDate= Calendar.getInstance();
             SimpleDateFormat currentDate = new SimpleDateFormat("dd-MMMM-YYYY");
            final string saveCurrentDate = currentDate.format(calForDate.getTime());
@@ -93,17 +143,24 @@ public class Writecomment extends AppCompatActivity {
 
             final string RandomKey = saveCurrentDate + saveCurrentTime;*/
 
-            HashMap commentMap = new HashMap();
-            commentMap.put("Comment",commentText);
-            //commentMap.put("date",saveCurrentDate);
-           // commentMap.put("time",saveCurrentTime);
+               /*     HashMap commentMap = new HashMap();
+                    commentMap.put("Comment", commentText);
+                    //commentMap.put("date",saveCurrentDate);
+                    // commentMap.put("time",saveCurrentTime);
 
 
-
-
+                }
+                }
+            }/*
 
         }
-        
+
+
+
+    private void sendViewActivity()
+    {
+        Intent mainIntent = new Intent(Writecomment.this,Viewcomment.class);
+        startActivity(mainIntent);
     }
 
     private void StoringMessageFirebaseStorage(){
@@ -119,6 +176,53 @@ public class Writecomment extends AppCompatActivity {
 
 
         //  StorageReference filePath = PostMassageReference.child("Post Messages").child(url.getLastPathSegment());
+    }
+
+
+
+    private void validatecomment(String username) {
+        String commentText = commentInputText.getText().toString();
+        if(TextUtils.isEmpty(commentText))
+        {
+            Toast.makeText(this,"Please write text to comment...", LENGTH_SHORT).show();
+        }
+        else
+        {
+            Calendar calForDate = Calendar.getInstance();
+            SimpleDateFormat currentDate = new SimpleDateFormat("dd-MMMM-YYYY");
+            saveCurrentDate = currentDate.format(calForDate.getTime());
+
+            Calendar calForTime = Calendar.getInstance();
+            SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm");
+            saveCurrentTime = currentTime.format(calForTime.getTime());
+
+            RandomKey = current_user_id + saveCurrentDate + saveCurrentTime;
+
+            HashMap commentMap = new HashMap();
+                commentMap.put("uid",current_user_id);
+                commentMap.put("Comment", commentText);
+                commentMap.put("date", saveCurrentDate);
+                commentMap.put("time",saveCurrentTime);
+                commentMap.put("username",username);
+            PostRef.child(RandomKey).updateChildren(commentMap)
+                    .addOnCompleteListener(new OnCompleteListener() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+                            if(task.isSuccessful())
+                            {
+                               // SendToViewComment();
+                                Toast.makeText(Writecomment.this,"You have commented successfully...",Toast.LENGTH_LONG).show();
+
+                            }
+
+                            else
+                            {
+                                Toast.makeText(Writecomment.this,"Error occured, try again... ", LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+        }
     }
 
 
