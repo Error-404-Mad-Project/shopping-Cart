316af747c8c54c52461ea78b9c9d1b45c40ea588
paboda <pabodabalasooriya@gmail.com>
2020-04-22 22:06:52 +0530

    add new product activity and database connection is completed.

---
+++ app/src/main/java/com/e/androidproject/MainActivity.java
@@ -0,0 +1,37 @@
+package com.e.androidproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.e.androidproject.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void nextFunction(View view)
+    {
+            Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+            startActivity(intent);
+    }
+   // Write a message to the database
+   public void addproductFunction(View view)
+   {
+            Intent intent = new Intent(MainActivity.this, AdminCategoryActivity.class);
+            startActivity(intent);
+   }
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
