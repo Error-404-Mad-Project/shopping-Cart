905a0b5148332c4fbab6d3071ae84aeb641b55bd
ayeshani <ayeshanikaushalya@gmail.com>
2020-04-30 23:22:29 +0530

    Changing CRUD

--- app/src/main/java/com/shoppingcart/WriteComment.java
+++ app/src/main/java/com/shoppingcart/WriteComment.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -169,6 +171,23 @@ public class WriteComment extends AppCompatActivity {
             }
         });
     }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.customer_view,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
 
 
